df0b097ad0543f3f04c3e6103fa2b81e4b03babf
Piyush Saini <piyushsaini1226@gmail.com>
2025-10-14 12:58:04 +0530

    raw done

--- Backend/Server.js
+++ Backend/Server.js
@@ -16,7 +16,8 @@ app.post('/data', async (req, res) => {
   try {
   const data = await main(message)
   res.json({Metric:data.Metrices_Data, Raw:data.Raw_Data});
-  console.log(data.Metrices_Data);
+  console.log('Audit Completed');
+  // console.log(data.Metrices_Data);
   // console.log(data.Raw_Data);
     
   } catch (error) {
